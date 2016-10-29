e441ac4514ac69a624eba6336c5a1cf35db64e22
xiaojunjie <971308896@qq.com>
2016-10-29 11:10:27 +0800

    send from my crawler

--- crawler/spider/test.py
+++ crawler/spider/test.py
@@ -7,12 +7,12 @@ url = {
 	"rs": "http://rs.xidian.edu.cn/forum.php?mod=forumdisplay&fid=106",
 	"data": "_data/rs.yml",
 	"temp": "img_template.txt",
-	"log" : "git.log"
+	"log" : "git.log",
+	"img" : "../img"
 }
 
-dat  = open(url["git"]+url["data"],"w+") 
-temp = open(url["temp"],"r")
-yml  = str(temp.read())+"\r\n\r\n"
+
+
 
 def git():
 	shell = [
@@ -29,24 +29,31 @@ def git():
 
 def outlog(txt):
 	log  = open(url["log"],"a+")
-	log.write(time.strftime("\r\n\r\n----%Y-%m-%d %H:%M:%S-------\r\n\r\n", time.localtime()))
+	log.write(time.strftime("\r\n\r\n----%Y-%m-%d %H:%M:%S-------\r\n", time.localtime()))
 	log.write(txt)
 	log.close()
+
+def outdata(html):
+	dat  = open(url["git"]+url["data"],"w+")
+	temp = open(url["temp"],"r")
+	yml  = str(temp.read())+"\r\n\r\n"
+	bsObj = BeautifulSoup(html,"lxml")
+	for img in bsObj.findAll("img"):
+		if 'href' in img.parent.attrs and img["src"]!="" and img["alt"]!="":
+			dat.write(yml % (img["alt"],img["src"],img.parent.attrs['href']))
+	dat.close() 
+	temp.close()
 
 try:
 	html = urlopen(url['rs'])
 except Exception as e:
 	outlog(e)
 else:
-	bsObj = BeautifulSoup(html.read(),"lxml")
-	# print(bsObj.findAll(id="portal_block_343"));
-	for img in bsObj.findAll("img"):
-		if 'href' in img.parent.attrs and img["src"]!="" and img["alt"]!="":
-			dat.write(yml % (img["alt"],img["src"],img.parent.attrs['href']))
+	outdata(html.read())
 finally:
 	git()
 
-dat.close()
-temp.close()
+
+
 
 
